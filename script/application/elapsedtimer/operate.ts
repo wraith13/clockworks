5984fad4f8f7c441169272502f9af2b349475a07
道化師 <wraith13@hotmail.co.jp>
2021-10-16 00:22:40 +0900

    🛠 実装 ( WIP )

---
+++ script/application/elapsedtimer/operate.ts
@@ -0,0 +1,123 @@
+import { Locale } from "../../locale";
+import { Type } from "../../type";
+import { Base } from "../../base";
+import { Storage } from "../../storage";
+import { Domain } from "../../domain";
+import { Render } from "../../render";
+export module Operate
+{
+    export const newEvent = async (title: string, tick: number, onCanceled?: () => unknown) =>
+    {
+        const event: Type.EventEntry =
+        {
+            title,
+            tick,
+        };
+        Storage.ElapsedTimer.Events.add(event);
+        Render.updateWindow("operate");
+        const toast = Render.makePrimaryToast
+        ({
+            content: Render.$span("")(`${Locale.map("Saved!")}`),
+            backwardOperator: Render.cancelTextButton
+            (
+                async () =>
+                {
+                    Storage.ElapsedTimer.Events.remove(event);
+                    Render.updateWindow("operate");
+                    await toast.hide();
+                    onCanceled?.();
+                }
+            ),
+        });
+    };
+    export const save = async (item: Type.EventEntry, onCanceled?: () => unknown) =>
+    {
+        Storage.ElapsedTimer.Events.add(item);
+        Render.updateWindow("operate");
+        const toast = Render.makePrimaryToast
+        ({
+            content: Render.$span("")(`${Locale.map("Saved!")}`),
+            backwardOperator: Render.cancelTextButton
+            (
+                async () =>
+                {
+                    Storage.ElapsedTimer.Events.remove(item);
+                    Render.updateWindow("operate");
+                    await toast.hide();
+                    onCanceled?.();
+                }
+            ),
+        });
+    };
+    export const edit = async (item: Type.EventEntry, title: string, tick: number, onCanceled?: () => unknown) =>
+    {
+        const oldSchedule = item;
+        const newSchedule: Type.EventEntry =
+        {
+            title,
+            tick,
+        };
+        Storage.ElapsedTimer.Events.remove(oldSchedule);
+        Storage.ElapsedTimer.Events.add(newSchedule);
+        Render.updateWindow("operate");
+        const toast = Render.makePrimaryToast
+        ({
+            content: Render.$span("")(`${Locale.map("Saved!")}`),
+            backwardOperator: Render.cancelTextButton
+            (
+                async () =>
+                {
+                    Storage.ElapsedTimer.Events.remove(newSchedule);
+                    Storage.ElapsedTimer.Events.add(oldSchedule);
+                    Render.updateWindow("operate");
+                    await toast.hide();
+                    onCanceled?.();
+                }
+            ),
+        });
+    };
+    export const remove = async (item: Type.EventEntry, onCanceled?: () => unknown) =>
+    {
+        // const urlParams = getUrlParams(location.href)["item"];
+        const isOpend = !! Base.getUrlHash(location.href).split("/")[1];
+        Storage.ElapsedTimer.Events.remove(item);
+        if (isOpend)
+        {
+            Render.showUrl({ application: "ElapsedTimer", });
+        }
+        else
+        {
+            Render.updateWindow("operate");
+        }
+        const toast = Render.makePrimaryToast
+        ({
+            content: Render.$span("")(`${Locale.map("Removed.")}`),
+            backwardOperator: Render.cancelTextButton
+            (
+                async () =>
+                {
+                    Storage.ElapsedTimer.Events.add(item);
+                    if (isOpend)
+                    {
+                        Render.showUrl(Domain.makePageParams("ElapsedTimer", item));
+                    }
+                    else
+                    {
+                        Render.updateWindow("operate");
+                    }
+                    await toast.hide();
+                    onCanceled?.();
+                }
+            ),
+        });
+    };
+    export const removeAllEvents = async () =>
+    {
+        if (Render.systemConfirm(Locale.map("This action cannot be undone. Do you want to continue?")))
+        {
+            Storage.ElapsedTimer.Events.removeKey();
+            Render.updateWindow("operate");
+            Render.makePrimaryToast({ content: Render.$span("")(`${Locale.map("Removed all alarms!")}`), });
+        }
+    };
+}
--- script/render/operate.ts
+++ script/render/operate.ts
@@ -1,12 +1,12 @@
 import { minamo } from "../minamo.js";
 import { Locale } from "../locale";
-import { Type } from "../type";
 import { Base } from "../base";
 import { Storage } from "../storage";
 import { Domain } from "../domain";
 import { Render } from "./";
 import { Operate as RainbowClockOperate } from "../application/rainbowclock/operate";
 import { Operate as CountdownTimerOperate } from "../application/countdowntimer/operate";
+import { Operate as ElapsedTimerOperate } from "../application/elapsedtimer/operate";
 export module Operate
 {
     export const copyToClipboard = (text: string, displayText = `"${text}"`) =>
@@ -23,6 +23,7 @@ export module Operate
     };
     export const RainbowClock = RainbowClockOperate;
     export const CountdownTimer = CountdownTimerOperate;
+    export const ElapsedTimer = ElapsedTimerOperate;
     export module NeverStopwatch
     {
         export const stamp = async (tick: number, onCanceled?: () => unknown) =>
@@ -129,123 +130,6 @@ export module Operate
                 Storage.NeverStopwatch.Stamps.removeKey();
                 Render.updateWindow("operate");
                 Render.makePrimaryToast({ content: Render.$span("")(`${Locale.map("Removed all stamps!")}`), });
-            }
-        };
-    }
-    export module ElapsedTimer
-    {
-        export const newEvent = async (title: string, tick: number, onCanceled?: () => unknown) =>
-        {
-            const event: Type.EventEntry =
-            {
-                title,
-                tick,
-            };
-            Storage.ElapsedTimer.Events.add(event);
-            Render.updateWindow("operate");
-            const toast = Render.makePrimaryToast
-            ({
-                content: Render.$span("")(`${Locale.map("Saved!")}`),
-                backwardOperator: Render.cancelTextButton
-                (
-                    async () =>
-                    {
-                        Storage.ElapsedTimer.Events.remove(event);
-                        Render.updateWindow("operate");
-                        await toast.hide();
-                        onCanceled?.();
-                    }
-                ),
-            });
-        };
-        export const save = async (item: Type.EventEntry, onCanceled?: () => unknown) =>
-        {
-            Storage.ElapsedTimer.Events.add(item);
-            Render.updateWindow("operate");
-            const toast = Render.makePrimaryToast
-            ({
-                content: Render.$span("")(`${Locale.map("Saved!")}`),
-                backwardOperator: Render.cancelTextButton
-                (
-                    async () =>
-                    {
-                        Storage.ElapsedTimer.Events.remove(item);
-                        Render.updateWindow("operate");
-                        await toast.hide();
-                        onCanceled?.();
-                    }
-                ),
-            });
-        };
-        export const edit = async (item: Type.EventEntry, title: string, tick: number, onCanceled?: () => unknown) =>
-        {
-            const oldSchedule = item;
-            const newSchedule: Type.EventEntry =
-            {
-                title,
-                tick,
-            };
-            Storage.ElapsedTimer.Events.remove(oldSchedule);
-            Storage.ElapsedTimer.Events.add(newSchedule);
-            Render.updateWindow("operate");
-            const toast = Render.makePrimaryToast
-            ({
-                content: Render.$span("")(`${Locale.map("Saved!")}`),
-                backwardOperator: Render.cancelTextButton
-                (
-                    async () =>
-                    {
-                        Storage.ElapsedTimer.Events.remove(newSchedule);
-                        Storage.ElapsedTimer.Events.add(oldSchedule);
-                        Render.updateWindow("operate");
-                        await toast.hide();
-                        onCanceled?.();
-                    }
-                ),
-            });
-        };
-        export const remove = async (item: Type.EventEntry, onCanceled?: () => unknown) =>
-        {
-            // const urlParams = getUrlParams(location.href)["item"];
-            const isOpend = !! Base.getUrlHash(location.href).split("/")[1];
-            Storage.ElapsedTimer.Events.remove(item);
-            if (isOpend)
-            {
-                Render.showUrl({ application: "ElapsedTimer", });
-            }
-            else
-            {
-                Render.updateWindow("operate");
-            }
-            const toast = Render.makePrimaryToast
-            ({
-                content: Render.$span("")(`${Locale.map("Removed.")}`),
-                backwardOperator: Render.cancelTextButton
-                (
-                    async () =>
-                    {
-                        Storage.ElapsedTimer.Events.add(item);
-                        if (isOpend)
-                        {
-                            Render.showUrl(Domain.makePageParams("ElapsedTimer", item));
-                        }
-                        else
-                        {
-                            Render.updateWindow("operate");
-                        }
-                        await toast.hide();
-                        onCanceled?.();
-                    }
-                ),
-            });
-        };
-        export const removeAllEvents = async () =>
-        {
-            if (Render.systemConfirm(Locale.map("This action cannot be undone. Do you want to continue?")))
-            {
-                Storage.ElapsedTimer.Events.removeKey();
-                Render.updateWindow("operate");
-                Render.makePrimaryToast({ content: Render.$span("")(`${Locale.map("Removed all alarms!")}`), });
             }
         };
     }
