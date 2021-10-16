8a367018447adc836bd51a5b4b65c38e4b13b7d4
道化師 <wraith13@hotmail.co.jp>
2021-10-16 15:06:09 +0900

    🛠 実装 ( WIP )

---
+++ script/application/neverstopwatch/operate.ts
@@ -0,0 +1,114 @@
+import { Locale } from "../../locale";
+import { Base } from "../../base";
+import { Storage } from "../../storage";
+import { Domain } from "../../domain";
+import { Render } from "../../render";
+export module Operate
+{
+    export const stamp = async (tick: number, onCanceled?: () => unknown) =>
+    {
+        const backup = Storage.NeverStopwatch.Stamps.get();
+        Storage.NeverStopwatch.Stamps.add(tick);
+        Render.updateWindow("operate");
+        const toast = Render.makePrimaryToast
+        ({
+            content: Render.$span("")(`${Locale.map("Stamped!")}`),
+            backwardOperator: Render.cancelTextButton
+            (
+                async () =>
+                {
+                    Storage.NeverStopwatch.Stamps.set(backup);
+                    Render.updateWindow("operate");
+                    await toast.hide();
+                    onCanceled?.();
+                }
+            ),
+        });
+    };
+    export const save = async (tick: number, onCanceled?: () => unknown) =>
+    {
+        const backup = Storage.NeverStopwatch.Stamps.get();
+        Storage.NeverStopwatch.Stamps.add(tick);
+        Render.updateWindow("operate");
+        const toast = Render.makePrimaryToast
+        ({
+            content: Render.$span("")(`${Locale.map("Saved!")}`),
+            backwardOperator: Render.cancelTextButton
+            (
+                async () =>
+                {
+                    Storage.NeverStopwatch.Stamps.set(backup);
+                    Render.updateWindow("operate");
+                    await toast.hide();
+                    onCanceled?.();
+                }
+            ),
+        });
+    };
+    export const edit = async (oldTick: number, newTick: number, onCanceled?: () => unknown) =>
+    {
+        const backup = Storage.NeverStopwatch.Stamps.get();
+        Storage.NeverStopwatch.Stamps.remove(oldTick);
+        Storage.NeverStopwatch.Stamps.add(newTick);
+        Render.updateWindow("operate");
+        const toast = Render.makePrimaryToast
+        ({
+            content: Render.$span("")(`${Locale.map("Updated.")}`),
+            backwardOperator: Render.cancelTextButton
+            (
+                async () =>
+                {
+                    Storage.NeverStopwatch.Stamps.set(backup);
+                    Render.updateWindow("operate");
+                    await toast.hide();
+                    onCanceled?.();
+                }
+            ),
+        });
+    };
+    export const removeStamp = async (tick: number, onCanceled?: () => unknown) =>
+    {
+        // const urlParams = getUrlParams(location.href)["item"];
+        const isOpend = !! Base.getUrlHash(location.href).split("/")[1];
+        const backup = Storage.NeverStopwatch.Stamps.get();
+        Storage.NeverStopwatch.Stamps.remove(tick);
+        if (isOpend)
+        {
+            Render.showUrl({ application: "NeverStopwatch", });
+        }
+        else
+        {
+            Render.updateWindow("operate");
+        }
+        const toast = Render.makePrimaryToast
+        ({
+            content: Render.$span("")(`${Locale.map("Removed.")}`),
+            backwardOperator: Render.cancelTextButton
+            (
+                async () =>
+                {
+                    Storage.NeverStopwatch.Stamps.set(backup);
+                    if (isOpend)
+                    {
+                        Render.showUrl(Domain.makePageParams("NeverStopwatch", tick));
+                    }
+                    else
+                    {
+                        Render.updateWindow("operate");
+                    }
+                    await toast.hide();
+                    onCanceled?.();
+                }
+            ),
+        });
+    };
+    export const removeAllStamps = async () =>
+    {
+        if (Render.systemConfirm(Locale.map("This action cannot be undone. Do you want to continue?")))
+        {
+            Storage.NeverStopwatch.Stamps.removeKey();
+            Render.updateWindow("operate");
+            Render.makePrimaryToast({ content: Render.$span("")(`${Locale.map("Removed all stamps!")}`), });
+        }
+    };
+}
--- script/render/operate.ts
+++ script/render/operate.ts
@@ -1,12 +1,9 @@
 import { minamo } from "../minamo.js";
-import { Locale } from "../locale";
-import { Base } from "../base";
-import { Storage } from "../storage";
-import { Domain } from "../domain";
 import { Render } from "./";
 import { Operate as RainbowClockOperate } from "../application/rainbowclock/operate";
 import { Operate as CountdownTimerOperate } from "../application/countdowntimer/operate";
 import { Operate as ElapsedTimerOperate } from "../application/elapsedtimer/operate";
+import { Operate as NeverStopwatchOperate } from "../application/neverstopwatch/operate";
 export module Operate
 {
     export const copyToClipboard = (text: string, displayText = `"${text}"`) =>
@@ -24,113 +21,5 @@ export module Operate
     export const RainbowClock = RainbowClockOperate;
     export const CountdownTimer = CountdownTimerOperate;
     export const ElapsedTimer = ElapsedTimerOperate;
-    export module NeverStopwatch
-    {
-        export const stamp = async (tick: number, onCanceled?: () => unknown) =>
-        {
-            const backup = Storage.NeverStopwatch.Stamps.get();
-            Storage.NeverStopwatch.Stamps.add(tick);
-            Render.updateWindow("operate");
-            const toast = Render.makePrimaryToast
-            ({
-                content: Render.$span("")(`${Locale.map("Stamped!")}`),
-                backwardOperator: Render.cancelTextButton
-                (
-                    async () =>
-                    {
-                        Storage.NeverStopwatch.Stamps.set(backup);
-                        Render.updateWindow("operate");
-                        await toast.hide();
-                        onCanceled?.();
-                    }
-                ),
-            });
-        };
-        export const save = async (tick: number, onCanceled?: () => unknown) =>
-        {
-            const backup = Storage.NeverStopwatch.Stamps.get();
-            Storage.NeverStopwatch.Stamps.add(tick);
-            Render.updateWindow("operate");
-            const toast = Render.makePrimaryToast
-            ({
-                content: Render.$span("")(`${Locale.map("Saved!")}`),
-                backwardOperator: Render.cancelTextButton
-                (
-                    async () =>
-                    {
-                        Storage.NeverStopwatch.Stamps.set(backup);
-                        Render.updateWindow("operate");
-                        await toast.hide();
-                        onCanceled?.();
-                    }
-                ),
-            });
-        };
-        export const edit = async (oldTick: number, newTick: number, onCanceled?: () => unknown) =>
-        {
-            const backup = Storage.NeverStopwatch.Stamps.get();
-            Storage.NeverStopwatch.Stamps.remove(oldTick);
-            Storage.NeverStopwatch.Stamps.add(newTick);
-            Render.updateWindow("operate");
-            const toast = Render.makePrimaryToast
-            ({
-                content: Render.$span("")(`${Locale.map("Updated.")}`),
-                backwardOperator: Render.cancelTextButton
-                (
-                    async () =>
-                    {
-                        Storage.NeverStopwatch.Stamps.set(backup);
-                        Render.updateWindow("operate");
-                        await toast.hide();
-                        onCanceled?.();
-                    }
-                ),
-            });
-        };
-        export const removeStamp = async (tick: number, onCanceled?: () => unknown) =>
-        {
-            // const urlParams = getUrlParams(location.href)["item"];
-            const isOpend = !! Base.getUrlHash(location.href).split("/")[1];
-            const backup = Storage.NeverStopwatch.Stamps.get();
-            Storage.NeverStopwatch.Stamps.remove(tick);
-            if (isOpend)
-            {
-                Render.showUrl({ application: "NeverStopwatch", });
-            }
-            else
-            {
-                Render.updateWindow("operate");
-            }
-            const toast = Render.makePrimaryToast
-            ({
-                content: Render.$span("")(`${Locale.map("Removed.")}`),
-                backwardOperator: Render.cancelTextButton
-                (
-                    async () =>
-                    {
-                        Storage.NeverStopwatch.Stamps.set(backup);
-                        if (isOpend)
-                        {
-                            Render.showUrl(Domain.makePageParams("NeverStopwatch", tick));
-                        }
-                        else
-                        {
-                            Render.updateWindow("operate");
-                        }
-                        await toast.hide();
-                        onCanceled?.();
-                    }
-                ),
-            });
-        };
-        export const removeAllStamps = async () =>
-        {
-            if (Render.systemConfirm(Locale.map("This action cannot be undone. Do you want to continue?")))
-            {
-                Storage.NeverStopwatch.Stamps.removeKey();
-                Render.updateWindow("operate");
-                Render.makePrimaryToast({ content: Render.$span("")(`${Locale.map("Removed all stamps!")}`), });
-            }
-        };
-    }
+    export const NeverStopwatch = NeverStopwatchOperate;
 }
