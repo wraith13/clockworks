bc4d393a582a1c8deadd329355d537e77f1c819d
道化師 <wraith13@hotmail.co.jp>
2022-03-09 22:22:51 +0900

    🛠 実装 ( WIP )

--- tektite.js/script/tektite-view-model.ts
+++ tektite.js/script/tektite-view-model.ts
@@ -9,6 +9,7 @@ export module ViewModel
         lastAccess: number;
         type: ViewModelTypeName;
         data: string;
+        childrenKeys: string[];
     }
     export class ViewModel<PageParams, IconKeyType, LocaleEntryType extends Tektite.LocaleEntry, LocaleMapType extends { [language: string]: LocaleEntryType }>
     {
@@ -32,6 +33,7 @@ export module ViewModel
         };
         public renderDom = (now: number = new Date().getTime()) =>
         {
+            let result: Element;
             const json = JSON.stringify(this.data);
             if (this.previousData !== json)
             {
@@ -50,13 +52,14 @@ export module ViewModel
                         };
                     }
                 );
-                this.renderOrCache(now, "/", JSON.parse(json));
+                result = this.renderOrCache(now, "/", JSON.parse(json)).dom;
                 Object.keys(oldCache)
                     .filter(path => oldCache[path].dom !== this.domCache[path]?.dom)
                     .map(path => oldCache[path].dom)
                     .forEach(dom => dom.parentElement.removeChild(dom));
                 this.previousData = json;
             }
+            return result;
         };
         private domCache: { [path: string]:DomCache<unknown> } = { };
         public getCache = (now: number, path: string) =>
@@ -71,28 +74,49 @@ export module ViewModel
         public getChildren = (key: string) => Object.keys(this.domCache)
             .filter(i => i.startsWith(key) && 2 === i.substring(i.length).split["/"].length)
             .map(i => this.domCache[i]);
-        public setCache = (now: number, key: string, dom: Element, type: ViewModelTypeName, data: string) =>
+        public setCache = (now: number, key: string, dom: Element, type: ViewModelTypeName, data: string, childrenKeys: string[]) =>
             this.domCache[key] =
             {
                 dom,
                 lastAccess: now,
                 type,
                 data,
+                childrenKeys
             };
         public renderOrCache = (now: number, path: string, data: Tektite.ViewModelBase<unknown>): DomCache<unknown> =>
         {
             let cache = this.getCache(now, path);
+            const oldChildrenKeys = cache?.childrenKeys ?? [];
+            const childrenKeys = data.children.map(i => i.key);
+            const renderer = this.renderer[data.type];
+            const forceAppend = renderer.isListContainer && this.isSameOrder(oldChildrenKeys, childrenKeys);
             const json = JSON.stringify(data.data);
             if (cache?.data !== json)
             {
-                const renderer = this.renderer[data.type];
                 let dom = cache.dom ?? renderer.make();
                 dom = renderer.update(cache, data);
-                cache = this.setCache(now, path, dom, data.type, json);
+                cache = this.setCache(now, path, dom, data.type, json, childrenKeys);
             }
-            data.children.forEach(i => this.renderOrCache(now, `${path}/${i.key}`, i));
+            const children = data.children.map
+            (
+                i =>
+                {
+                    const c = this.renderOrCache(now, `${path}/${i.key}`, i);
+                    const container = renderer.getChildModelContainer(i.key);
+                    if (forceAppend || container !== c.dom.parentElement)
+                    {
+                        container.appendChild(c.dom)
+                    }
+                    return c;
+                }
+            );
             return cache;
         };
+        public isSameOrder = (old: string[], now: string[]) =>
+        {
+            const filteredOld = old.filter(i => 0 <= now.indexOf(i));
+            return old.filter((i,ix) => i !== now[ix]).length <= 0;
+        }
     }
     export const make = <PageParams, IconKeyType, LocaleEntryType extends Tektite.LocaleEntry, LocaleMapType extends { [language: string]: LocaleEntryType }>(tektite: Tektite.Tektite<PageParams, IconKeyType, LocaleEntryType, LocaleMapType>) =>
         new ViewModel(tektite);
