78569a19d5969b39a08e5d51d16630ee022045e6
道化師 <wraith13@hotmail.co.jp>
2022-03-12 00:47:31 +0900

    🛠 実装 ( WIP )

--- tektite.js/script/tektite-view-model.ts
+++ tektite.js/script/tektite-view-model.ts
@@ -15,7 +15,8 @@ export module ViewModel
     {
         private previousData: string;
         private data: Tektite.ViewModelBase<unknown>;
-        private renderer: { [type: string ]: Tektite.ViewRenderer<unknown>};
+        private renderer: { [type: string ]: Tektite.ViewRenderer<unknown, Tektite.ViewModelBase<unknown>>};
+        private unknownRenderer: Tektite.ViewRenderer<unknown, Tektite.ViewModelBase<unknown>>;
         constructor(public tektite: Tektite.Tektite<PageParams, IconKeyType, LocaleEntryType, LocaleMapType>)
         {
         }
@@ -25,13 +26,54 @@ export module ViewModel
         };
         public set = (data: Tektite.ViewModelBase<unknown>) =>
         {
-            this.data = data;
+            if (this.hasDuplicatedKeys(data))
+            {
+                console.error(`tektite-view-model: Duplicated keys - data:${data}`);
+            }
+            else
+            {
+                this.data = data;
+            }
         };
         public get = (path: string = "/") =>
         {
-            return this.data;
+            if ("/" === path)
+            {
+                return this.data;
+            }
+            else
+            {
+                const keys = path.split("/").filter(i => 0 < i.length);
+                let current: Tektite.ViewModelBase<unknown>;
+                if (0 < keys.length)
+                {
+                    current = { children: [ this.data ] } as Tektite.ViewModelBase<unknown>;
+                    while(0 < keys.length)
+                    {
+                        current = (current.children ?? []).filter(i => i.key === keys[0])[0];
+                        if ( ! current)
+                        {
+                            console.error(`tektite-view-model: Path not found - path:${path}`);
+                            break;
+                        }
+                        keys.shift();
+                    }
+                }
+                else
+                {
+                    console.error(`tektite-view-model: Path not found - path:${path}`);
+                }
+                return current;
+            }
         };
-        public renderDom = (now: number = new Date().getTime()) =>
+        public hasInvalidViewModel = (data: Tektite.ViewModelBase<unknown>) =>
+            "" === (data?.key ?? "") ||
+            "" === (data?.type ?? "") ||
+            0 < (data?.children ?? []).filter(i => this.hasInvalidViewModel(i)).length;
+        public hasDuplicatedKeys = (data: Tektite.ViewModelBase<unknown>) =>
+            0 < (data?.children ?? []).map(i => i.key).filter((i, ix, list) => 0 <= list.indexOf(i, ix +1)).length ||
+            0 < (data?.children ?? []).filter(i => this.hasDuplicatedKeys(i)).length;
+        public renderDom = async (now: number = new Date().getTime()) =>
         {
             let result: Element;
             const json = JSON.stringify(this.data);
@@ -53,12 +95,19 @@ export module ViewModel
                     }
                 );
                 const data = JSON.parse(json) as Tektite.ViewModelBase<unknown>;
-                result = this.renderOrCache(now, `/${data?.key}`, data).dom;
-                Object.keys(oldCache)
-                    .filter(path => oldCache[path].dom !== this.domCache[path]?.dom)
-                    .map(path => oldCache[path].dom)
-                    .forEach(dom => dom.parentElement.removeChild(dom));
-                this.previousData = json;
+                if (this.hasDuplicatedKeys(data))
+                {
+                    console.error(`tektite-view-model: Duplicated keys - data:${data}`);
+                }
+                else
+                {
+                    result = (await this.renderOrCache(now, `/${data?.key}`, data)).dom;
+                    Object.keys(oldCache)
+                        .filter(path => oldCache[path].dom !== this.domCache[path]?.dom)
+                        .map(path => oldCache[path].dom)
+                        .forEach(dom => dom.parentElement.removeChild(dom));
+                    this.previousData = json;
+                }
             }
             return result;
         };
@@ -84,47 +133,51 @@ export module ViewModel
                 data,
                 childrenKeys
             };
-        public renderOrCache = (now: number, path: string, data: Tektite.ViewModelBase<unknown>): DomCache<unknown> =>
+        public renderOrCache = async (now: number, path: string, data: Tektite.ViewModelBase<unknown>): Promise<DomCache<unknown>> =>
         {
             let cache = this.getCache(now, path);
             const oldChildrenKeys = cache?.childrenKeys ?? [];
             const childrenKeys = data?.children?.map(i => i.key) ?? [];
-            const renderer = this.renderer[data?.type];
+            const renderer = this.renderer[data?.type] ?? this.unknownRenderer;
             const forceAppend = renderer.isListContainer && this.isSameOrder(oldChildrenKeys, childrenKeys);
             const json = JSON.stringify(data?.data);
-            if ( ! data?.key)
+            const outputError = (message: string) => console.error(`tektite-view-model: ${message} - path:${path}, data:${JSON.stringify(data)}`);
+            if ( ! data?.key || "" === data.key)
             {
-                console.error(`tektite-view-model(${path}): It has not 'key' : ${JSON.stringify(data)}`);
+                outputError("It has not 'key'");
             }
             else
             if ( ! data?.type)
             {
-                console.error(`tektite-view-model(${path}): It has not 'type' : ${JSON.stringify(data)}`);
+                outputError("It has not 'type'");
             }
             else
             if ( ! renderer)
             {
-                console.error(`tektite-view-model(${path}): Unknown type '${data?.type}' : ${JSON.stringify(data)}`);
+                outputError("Unknown type '${data?.type}'");
             }
             else
             {
                 if (cache?.data !== json)
                 {
-                    let dom = cache.dom ?? renderer.make();
-                    dom = renderer.update(cache, data);
+                    let dom = cache.dom ?? await renderer.make();
+                    dom = await renderer.update(cache, data);
                     cache = this.setCache(now, path, dom, data.type, json, childrenKeys);
                 }
-                data.children.forEach
+                await Promise.all
                 (
-                    i =>
-                    {
-                        const c = this.renderOrCache(now, `${path}/${i.key}`, i);
-                        const container = renderer.getChildModelContainer(cache.dom, i.key);
-                        if (forceAppend || container !== c.dom.parentElement)
+                    data.children.map
+                    (
+                        async i =>
                         {
-                            container.appendChild(c.dom)
+                            const c = await this.renderOrCache(now, `${path}/${i.key}`, i);
+                            const container = renderer.getChildModelContainer(cache.dom, i.key);
+                            if (forceAppend || container !== c.dom.parentElement)
+                            {
+                                container.appendChild(c.dom)
+                            }
                         }
-                    }
+                    )
                 );
             }
             return cache;
