b30058800e8cc89b1be5d6ffd4bf9949e69da2cb
道化師 <wraith13@hotmail.co.jp>
2022-05-25 10:18:48 +0900

    🛠 実装 ( WIP )

--- tektite.js/script/tektite-view-command.ts
+++ tektite.js/script/tektite-view-command.ts
@@ -1,5 +1,6 @@
 import { minamo } from "../../nephila/minamo.js/index.js";
 import { Tektite } from "./tektite-index";
+import { ViewModel } from "./tektite-view-model.js";
 
 export module ViewCommand
 {
@@ -8,7 +9,16 @@ export module ViewCommand
         type: string;
         data?: minamo.core.JsonableObject;
     }
+    export interface ScrollToCommand extends EntryBase
+    {
+        type: "scroll-to";
+        data:
+        {
+            path: ViewModel.PathType;
+        }
+    }
     export type Entry = EntryBase | string;
+    export const getType = (entry: Entry) => "string" === typeof entry ? entry: entry.type;
     export type Command<OmegaEntry extends Entry> = (data: OmegaEntry) => unknown;
     export class ViewCommand<T extends Tektite.ParamTypes>
     {
@@ -16,7 +26,19 @@ export module ViewCommand
         constructor(public tektite: Tektite.Tektite<T>)
         {
         }
-        public readonly renderer: { [type: string ]: Command<any> } = { }
+        public execute(entry: Entry)
+        {
+            const executer = this.commands[getType(entry)];
+            if (executer)
+            {
+                executer(entry);
+            }
+            else
+            {
+                console.error(`tektite-view-model: Unknown command - data:${JSON.stringify(entry)}`);
+            }
+        }
+        public readonly commands: { [type: string ]: Command<any> } = { }
     }
     export const make = <T extends Tektite.ParamTypes>(tektite: Tektite.Tektite<T>) =>
         new ViewCommand(tektite);
